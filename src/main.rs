5329a503ee9aeea6421213b3e38ca7a47afdf874
jack.tricker@mac <jack.tricker@dunnhumby.com>
2025-04-02 17:20:38 +1100

    testing

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,20 @@ fn main() {
     let mut cpu = CPU::new();
     let mut memory = Memory::new();
 
-    // Load a test instruction into memory
-    memory.write(0x0100, 0x3C); // INC A
+    memory.write(0x0100, 0x08);
+    memory.write(0x0101, 0x00);
+    memory.write(0x0102, 0x01);
+
+
+
 
     
     // Emulation loop (one step for now)
     let opcode = cpu.fetch(&memory);
-    cpu.execute(opcode);
+    cpu.execute(opcode, &mut memory);
+
+
+
     println!("CPU State: A = {:#X}", cpu.a);
     println!("{}", cpu)
 }
