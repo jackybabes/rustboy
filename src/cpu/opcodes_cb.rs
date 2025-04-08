9f7043af0e3be9dc67d73029265e844cbcb15219
jack.tricker@mac <jack.tricker@dunnhumby.com>
2025-04-08 22:21:15 +1000

    cb shift left and right

--- src/cpu/opcodes_cb.rs
+++ src/cpu/opcodes_cb.rs
@@ -26,7 +26,16 @@ impl CPU {
                 let cycles = self.run_operation_on_index(memory, index, |cpu, value| cpu.rotate_right_carry_cb(value));
                 self.cycles += cycles
             }, // Rotate right carry
-
+            0x20..=0x27 => {
+                let index = opcode & 0x07;
+                let cycles = self.run_operation_on_index(memory, index, |cpu, value| cpu.shift_left_arithmetic_cb(value));
+                self.cycles += cycles
+            }, // Shift left arithmetic
+            0x28..=0x2F => {
+                let index = opcode & 0x07;
+                let cycles = self.run_operation_on_index(memory, index, |cpu, value| cpu.shift_right_arithmetic_cb(value));
+                self.cycles += cycles
+            }, // Shift right arithmetic
 
             
 
@@ -115,6 +124,31 @@ impl CPU {
 
         result
     }
+
+    fn shift_left_arithmetic_cb(&mut self, value: u8) -> u8 {
+        let bit7 = (value & 0x80) != 0;
+        let result = value << 1;
+    
+        self.set_flag(&Flag::Z, result == 0);
+        self.set_flag(&Flag::N, false);
+        self.set_flag(&Flag::H, false);
+        self.set_flag(&Flag::C, bit7);
+    
+        result
+    }
+
+    fn shift_right_arithmetic_cb(&mut self, value: u8) -> u8 {
+        let bit0 = (value & 0x01) != 0;
+        let result = value >> 1;
+    
+        self.set_flag(&Flag::Z, result == 0);
+        self.set_flag(&Flag::N, false);
+        self.set_flag(&Flag::H, false);
+        self.set_flag(&Flag::C, bit0);
+    
+        result
+    }
+    
     
     
         
