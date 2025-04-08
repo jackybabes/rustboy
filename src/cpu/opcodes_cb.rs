06a47dde22d551bac5b517be6e47fd27d0f924a4
jack.tricker@mac <jack.tricker@dunnhumby.com>
2025-04-08 22:15:43 +1000

    rotate through carry

--- src/cpu/opcodes_cb.rs
+++ src/cpu/opcodes_cb.rs
@@ -16,6 +16,18 @@ impl CPU {
                 let cycles = self.run_operation_on_index(memory, index, |cpu, value| cpu.rotate_right_circular_cb(value));
                 self.cycles += cycles
             }, // Rotate right circular
+            0x10..=0x17 => {
+                let index = opcode & 0x07;
+                let cycles = self.run_operation_on_index(memory, index, |cpu, value| cpu.rotate_left_carry_cb(value));
+                self.cycles += cycles
+            }, // Rotate left carry
+            0x18..=0x1F => {
+                let index = opcode & 0x07;
+                let cycles = self.run_operation_on_index(memory, index, |cpu, value| cpu.rotate_right_carry_cb(value));
+                self.cycles += cycles
+            }, // Rotate right carry
+
+
             
 
             _ => panic!("Invalid CB opcode: {:#X}", opcode),
@@ -79,5 +91,32 @@ impl CPU {
 
         result
     }
+
+    fn rotate_left_carry_cb(&mut self, value: u8) -> u8{
+        let bit7 = (value & 0x80) != 0;
+        let result = (value << 1 ) | self.get_flag(&Flag::C) as u8;
+    
+        self.set_flag(&Flag::Z, result == 0);
+        self.set_flag(&Flag::N, false);
+        self.set_flag(&Flag::H, false);
+        self.set_flag(&Flag::C, bit7);
+
+        result
+    }
+
+    fn rotate_right_carry_cb(&mut self, value: u8) -> u8{
+        let bit0 = (value & 0x01) != 0;
+        let result = (value >> 1) | (self.get_flag(&Flag::C) as u8) << 7;
+    
+        self.set_flag(&Flag::Z, result == 0);
+        self.set_flag(&Flag::N, false);
+        self.set_flag(&Flag::H, false);
+        self.set_flag(&Flag::C, bit0);
+
+        result
+    }
+    
+    
+        
 
 }
